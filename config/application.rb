6a37b9be142d94cf33f7ad9fc392adc52274276f
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-17 23:30:57 -0600

    moved precomp into class

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -54,6 +53,8 @@ module Rblog
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
