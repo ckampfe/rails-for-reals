119a84f635b435a10da80e699a2ae8fe767c84bd
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-17 22:22:26 -0600

    haven't commited in a while, oops

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,33 @@ class PostsController < ActionController::Base
   def index
     @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to :action => "show", :id => @post.id
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    redirect_to :action => "show", :id => @post.id
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+
 end
