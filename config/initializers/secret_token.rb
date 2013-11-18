a054be73ab9d87d7fc41e24d0806c540be843071
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-17 18:59:15 -0600

    initial, work on migration, model, views, controllers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rblog::Application.config.secret_token = '27ea79ff713f192ea161a9df804c0c166491656a27bd9e1bfcb3bccdaa008219ab079a0fc1ad6ab3773ffbb5993781624c2c7d5935034cf5795524dbc630e632'
