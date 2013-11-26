ecf6a1e552035fcb08f05d8a4b6a3f1b436bcf42
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-26 09:13:13 -0600

    method to total the length of all posts (over 40000)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
   attr_accessible :text, :title
+
+  def self.length_total
+    Rails.cache.fetch("length_total", :expires_in => 10.seconds) do
+      Post.all.reduce(0) { |acc, pos| acc + pos.text.length }
+    end
+  end
+
 end
